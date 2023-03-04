496e8398dd58435c5bccc9071dd84aaae3d9e799
animek12 <111511065+animek12@users.noreply.github.com>
2023-03-04 17:51:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,13 @@ function start() {
       })
 }
 start()
+import express from 'express'
+const app = express();
+  
+app.get('/', (req, res) => {
+  res.send('Hello Express app!')
+})
+ 
+app.listen(3000, () => {
+  console.log('server started')
+})
